6bc37ca49d5edd0e4f3097537e61ae57baf85da3
DESKTOP-GD314II\miyaz_ca1c3co <a.miyazawa1024@gmail.com>
2025-12-01 22:19:43 +0900

    プロジェクト ファイルを追加します。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace TaskManagerApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTask_Clik(object sender, EventArgs e)
+        {
+            var text = TaskTextBox.Text.Trim();
+            if (!string.IsNullOrEmpty(text))
+            {
+                TaskListBox.Items.Add(text);
+                TaskTextBox.Clear();
+            }
+        }
+    }
+}
